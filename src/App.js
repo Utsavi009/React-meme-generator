164677a5d274a87d291701aee03da5b8ce59dc88
Utsavi009 <utsavikathiriya@gmail.com>
2021-05-08 12:57:44 +0200

    update styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import domtoimage from 'dom-to-image';
 
 
-function App() {
+const App = () => {
 
   const [infos, setInfos] = useState([]);
   const [error, setError] = useState("");
@@ -13,10 +13,21 @@ function App() {
   const [bottomText, setBottomText] = useState('');
   const [randomImage, setRandomImage] = useState('https:\/\/i.imgflip.com\/30b1gx.jpg');
 
+  
+  useEffect(() => {
+    fetchData();
+  },[]);
+
+  const fetchData = async () => {
+    setLoading(true)
+    await axios.get(`https://api.imgflip.com/get_memes`)
+    .then(response => setInfos(response.data.data.memes))
+    .catch(error => setError(error))
+    setLoading(false)
+  }
+
   const node = document.getElementById('my-meme-image');
   const savedPlace = document.querySelector(".save-image");
-
-  console.log(node);
 
   const handleSaveImage = () => {
     domtoimage.toPng(node)
@@ -32,20 +43,7 @@ function App() {
   const resetSavedImage = () => {
     savedPlace.innerHTML = "";
   }
-    
-  useEffect(() => {
-    fetchData();
-  },[])  
-
-  const fetchData = async () => {
-    setLoading(true)
-    await axios.get(`https://api.imgflip.com/get_memes`)
-    .then(response => setInfos(response.data.data.memes))
-    .catch(error => setError(error))
-    setLoading(false)
-  }
   
-
   const chooseRandomImage = () => {
     let length = infos.length;
     let imageIndex = Math.floor(Math.random() * length);
@@ -66,24 +64,40 @@ function App() {
   };
 
   return (
-    <div className="app">
-      <div className='app-header'><h1> Meme Generator</h1> </div>
+
+    <div className="app container">
+      <div className='app-header'><h1 className='header-text'> Meme Generator</h1> </div>
+      <div className='main-container'>
       <div className='input-form'>
-            <input name='topText' placeholder='Enter Top Text' value={topText} onChange={handleChangeTop} />
-            <input name='bottomText' placeholder='Enter Bottom Text' value={bottomText} onChange={handleChangeBottom}/>  
+        <div className = 'input-container'>
+          <input name='topText' placeholder='Enter Top Text' value={topText} onChange={handleChangeTop} />
+          <input name='bottomText' placeholder='Enter Bottom Text' value={bottomText} onChange={handleChangeBottom}/> 
+        </div>
+        <div className='btn-container'> 
             <button onClick={chooseRandomImage}>Change</button>
             <button onClick={handleSaveImage}>Generate</button>
             <button onClick={resetSavedImage}>Reset</button>
-            <input type="file"  id="input-file" accept=".jpg, .jpeg, .png" onChange = {handleImageInputChange}></input>
+        </div>
+        <div className='file-container'>
+          <label for="input-file" class="custom-file-upload">
+            Custom Upload
+          </label>
+          <input type="file"  id="input-file" size="60"  accept=".jpg, .jpeg, .png" onChange = {handleImageInputChange}></input>
+        </div>
       </div>
+      <hr className='hr-style' />
+      <div className='image-container'>
       <div className='meme-image' id='my-meme-image'>
         <img src={randomImage} alt='mage' />
         <h2 className='top'>{topText}</h2>
         <h2 className='bottom'>{bottomText}</h2>
       </div>
       <div className='save-image'></div>
+      </div>
+      </div>
     </div>
+    
   );
 }
-/* https://imgflip.com/api */
+
 export default App;
